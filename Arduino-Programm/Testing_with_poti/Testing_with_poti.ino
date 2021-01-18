b5df7895c3ea78f7e498938f0184a986f6a18250
NotKet <52779314+NotKet@users.noreply.github.com>
2021-01-18 15:56:39 +0100

    updated

--- Arduino-Programm/Testing_with_poti/Testing_with_poti.ino
+++ Arduino-Programm/Testing_with_poti/Testing_with_poti.ino
@@ -6,7 +6,7 @@
  * 
 */
 
-// Initianlize Bluetooth
+// Initialize Bluetooth
 
 #include "BluetoothSerial.h"
 
@@ -14,23 +14,35 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it    
 #endif  
 
+#define calibration_min_value 1000
 #define min_sensor_value 2000
 #define measure_interval 1000
 
 BluetoothSerial SerialBT;
 
+
+
+// Declaring variables
+
 const long sensor1 = 34,sensor2 = 35, led = 33;
-unsigned long sensor1_value = 0, sensor1_start_value = 0, sensor2_value = 0, sensor2_start_value = 0, previousMillis = 0, sensor1_sum = 0, sensor2_sum = 0, sensor_sum_max = 0, sensor1_percentage = 0, sensor2_percentage = 0;
+unsigned long sensor1_value = 0, sensor1_start_value = 0, sensor2_value = 0, sensor2_start_value = 0, previousMillis = 0, sensor1_sum = 0, sensor2_sum = 0, sensor_sum_max = 0, sensor1_percentage = 0, sensor2_percentage = 0, calibration_sensor1 = 0, calibration_sensor2 = 0;
 bool measuring = false;
+
+unsigned long sensor1_arr [1000], sensor2_arr [1000];
 
 char app_input;
 
 
+
+
+// Starting a measurement
+
 void measurement() {
   measuring = true;
+  unsigned long count = 0;
   
   SerialBT.println("Messung wird gestartet. Bitte hänge dich nun an die Griffe ...");
-
+  
   while(1) {
     sensor1_start_value = analogRead(sensor1);
     sensor2_start_value = analogRead(sensor2);
@@ -46,19 +58,26 @@ void measurement() {
     sensor1_value = analogRead(sensor1);
     sensor2_value = analogRead(sensor2_value);
 
-    sensor1_sum += sensor1_value;
-    sensor2_sum += sensor2_value;
-
+    sensor1_arr[count] = sensor1_value;
+    sensor2_arr[count] = sensor2_value;
+    
     if(sensor1_value < 200 && sensor2_value < 200) {
-      measure = false;
+      measuring = false;
     }
+
+    count += 1;
   }
 
-  sensor_sum_max = sensor1_sum + sensor2+sum;
+  for(int i = 0; i <= count;  i += 1) {
+    sensor1_sum += sensor1_arr[i];
+    sensor2_sum += sensor2_arr[i];
+  }
 
-  sensor1_percentage = (sensor_sum_max / sensor1_sum) * 100;
-  sensor2_percentage = (sensor_sum_max / sensor2_sum) * 100;
+  sensor_sum_max = sensor1_sum + sensor2_sum;
 
+  sensor1_percentage = (sensor1_sum / sensor_sum_max) * 100;
+  sensor2_percentage = (sensor2_sum / sensor_sum_max) * 100;
+  
   SerialBT.print("Sensor 1 percentage: ");
   SerialBT.println(sensor1_percentage);
   SerialBT.print("Sensor 2 percentage: ");
@@ -66,11 +85,74 @@ void measurement() {
 }
 
 
+
+
+
+void date() {
+  
+}
+
+
+
+
+
+void calibration() {
+  SerialBT.println("Kalibrierung wird gestarted ...");
+  
+  while(1){
+    SerialBT.println("Bitte belaste nun Griff 1!");
+    calibration_sensor1 = analogRead(sensor1);
+    
+    if(calibration_sensor1 >= calibration_min_value) {
+      SerialBT.println("Sensor 1 funktioniert einwandfrei.");
+      break;
+    } else {
+      SerialBT.println("Ups! Etwas ist schiefgelaufen. Versuch es nochmal.");
+    }
+
+    delay(5000);
+  }
+
+  while(1) {
+    SerialBT.println("Bitte belaste nun Griff 2!");
+    calibration_sensor2 = analogRead(sensor2);
+
+    if(calibration_sensor2 >= calibration_min_value) {
+      SerialBT.println("Sensor 1 funktioniert einwandfrei.");
+      break;
+    } else {
+      SerialBT.println("Ups! Etwas ist schiefgelaufen. Versuch es nochmal.");
+    }
+
+    delay(5000);
+  }
+  
+}
+
+
+
+
+void arduino_version() {
+  
+}
+
+
+
+
+void battery_status() {
+  
+}
+
+
+// Initalize led, Serial Bluetooth Monitor, Serial Monitor
+
 void setup() {
-  pinMode(ledPin, INPUT);
+  pinMode(led, INPUT);
   Serial.begin(115200);
   SerialBT.begin("Hang'n Hold - 1");
 }
+
+
 
 
 void loop() {
