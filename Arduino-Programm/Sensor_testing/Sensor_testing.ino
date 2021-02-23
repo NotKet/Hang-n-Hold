cc8013eb942f73cb1e01364e302e2a8d1bba07ea
NotKet <52779314+NotKet@users.noreply.github.com>
2021-02-23 10:07:05 +0100

    updated

--- Arduino-Programm/Sensor_testing/Sensor_testing.ino
+++ Arduino-Programm/Sensor_testing/Sensor_testing.ino
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 const int piezoPin = 34;               
 
 void setup() {
@@ -12,72 +11,42 @@ void loop() {
   delay(200); 
 =======
 #include "BluetoothSerial.h"
-=======
->>>>>>> aa8f16cfbc70348c8436f6142acd07806d39a6e7
 #include "LedLib.h"
-#include "BluetoothSerial.h"
-#include "EEPROM.h"
-
-#define EEPROM_SIZE 1
-#define sm_value 115200
 
 BluetoothSerial SerialBT;
 
-const long sensorPin = 35, ledPin = 22, sensor1_value = 0, sensor2_value = 0;              
-unsigned long sensor_value = 0, ledstate = HIGH;
+const int sensorPin = 25, ledPin = 22;               
+unsigned long sensor_value = 0;
+
 boolean bt_connected = false;
 
-LedLib ll(ledPin);
+LedLib ledlib(ledPin);
 
-void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
-{
-  if(event == ESP_SPP_SRV_OPEN_EVT)
-  {
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  if(event == ESP_SPP_SRV_OPEN_EVT){
     Serial.println("Client connected.");
     bt_connected = true;
-    ll.led_on();
-    EEPROM.write(0, );
-    EEPROM.commit();
+    ledlib.led_on();
   } 
-  
-  if(event == ESP_SPP_CLOSE_EVT) 
-  {
+  if(event == ESP_SPP_CLOSE_EVT) {
     Serial.println("Client disconnected.");
     bt_connected = false;
   }
-}
-
-
-void setup() 
-{
-  Serial.begin(sm_value);
-  SerialBT.register_callback(callback);
-
-  pinMode(ledPin, OUTPUT);
-  EEPROM.begin(EEPROM_SIZE)
-  
-  if(!SerialBT.begin("Sensortest")) {
-    Serial.println("An error ocurred initializing Bluetooth");
-  } else {
-    Serial.println("Bluetooth initialized");
-  }
-
-  ledstate = EEPROM.read(0)
 } 
 
-<<<<<<< HEAD
+void setup() {
+  Serial.begin(115200);
+  SerialBT.register_callback(callback);
+  if(!SerialBT.begin("Hang'n Hold 1")){
+    Serial.println("An error occurred initializing Bluetooth");
+  }else{
+    Serial.println("Bluetooth initialized");
+  }
+} 
+
 void loop() {   
   while(bt_connected != true) {
     ledlib.blink_once();
   }
 >>>>>>> 88a934e6bec2ac2ade2e7e183ba74e72aa2dfaaa
-=======
-
-void loop() 
-{ 
-  if(!bt_connected)
-  {
-    ll.led_blink();
-  } 
->>>>>>> aa8f16cfbc70348c8436f6142acd07806d39a6e7
 } 
